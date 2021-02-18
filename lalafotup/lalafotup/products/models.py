0693cd370b5acba0f7fc128802e361354ba49d22
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-02-18 12:53:13 +0600

    change method ForeingKey Cascade

--- lalafotup/lalafotup/products/models.py
+++ lalafotup/lalafotup/products/models.py
@@ -11,4 +11,26 @@ class RuralGovernment(models.Model):
 class Village(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField(blank=True, null=True)
-    ruralgovernment = models.ForeignKey(RuralGovernment,on_delete=models.SET_NULL,null=True)
+    ruralgovernment = models.ForeignKey(RuralGovernment,on_delete=models.CASCADE,null=True)
+
+
+
+class Product(models.Model):
+    sub_categories = (
+        ('car','car'),
+        ('trucks','trucks'),
+        ('chicken','chicken'),
+        ('duck','duck'),
+        ('goose','goose'),
+        ('turkey','turkey'),
+    )
+    name = models.CharField(max_length=40)
+    description = models.TextField(blank=True,null=True)
+    sub_category = models.CharField(max_length=40,choices=sub_categories)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE,null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    village = models.ForeignKey(Village,on_delete=models.CASCADE,null=True)
