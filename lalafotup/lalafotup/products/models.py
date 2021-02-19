a939c7b958a8af68b212ecbbf115aaed2fb87e8f
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-02-19 14:43:19 +0600

    add field image from ruralgovernment, village, category and subcategory

--- lalafotup/lalafotup/products/models.py
+++ lalafotup/lalafotup/products/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class RuralGovernment(models.Model):
     name = models.CharField(max_length=40)
+    image = models.ImageField()
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
@@ -13,6 +14,7 @@ class RuralGovernment(models.Model):
 
 class Village(models.Model):
     name = models.CharField(max_length=40)
+    image = models.ImageField()
     description = models.TextField(blank=True, null=True)
     ruralgovernment = models.ForeignKey(RuralGovernment, on_delete=models.CASCADE)
 
@@ -33,6 +35,7 @@ class Product(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=40)
+    image = models.ImageField()
     village = models.ForeignKey(Village, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -41,6 +44,7 @@ class Category(models.Model):
 
 class SubCategory(models.Model):
     name = models.CharField(max_length=40)
+    image = models.ImageField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
