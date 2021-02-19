1ff28f841ec6682ed6b7c590d792900190eb845d
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-02-19 17:56:28 +0600

    add field phone from model Product

--- lalafotup/lalafotup/products/models.py
+++ lalafotup/lalafotup/products/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 
 class RuralGovernment(models.Model):
-    name = models.CharField(max_length=40)
-    image = models.ImageField(blank=True, null=True)
+    name = models.CharField(max_length=40, unique=True)
+    image = models.ImageField(blank=True, null=True, default='rural_default.jpg')
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
@@ -14,7 +14,7 @@ class RuralGovernment(models.Model):
 
 class Village(models.Model):
     name = models.CharField(max_length=40)
-    image = models.ImageField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True, default='defaultvillage.jpg')
     description = models.TextField(blank=True, null=True)
     ruralgovernment = models.ForeignKey(RuralGovernment, on_delete=models.CASCADE)
 
@@ -28,6 +28,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     subcategory = models.ForeignKey('SubCategory', on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
+    phone = models.IntegerField()
 
     def __str__(self):
         return self.name
@@ -35,7 +36,7 @@ class Product(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=40)
-    image = models.ImageField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True, default='Changing-default-category.jpg')
     village = models.ForeignKey(Village, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -44,7 +45,7 @@ class Category(models.Model):
 
 class SubCategory(models.Model):
     name = models.CharField(max_length=40)
-    image = models.ImageField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True, default='subcategorydefault.jpg')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
