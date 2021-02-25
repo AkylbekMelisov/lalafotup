bbe30b1f291bd9b52ceb7070e90a5cce6e375ca0
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-02-25 14:39:16 +0600

    add field description

--- lalafotup/lalafotup/products/forms.py
+++ lalafotup/lalafotup/products/forms.py
@@ -11,4 +11,4 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = ['name', 'image', 'phone', 'subcategory']
+        fields = ['name', 'image', 'phone', 'subcategory', 'description']
